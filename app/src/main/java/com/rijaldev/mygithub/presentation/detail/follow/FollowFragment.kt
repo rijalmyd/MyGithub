6afb37c8f38c493ba35f42d9f320fd50b47639d5
Rijal Muhyidin <71188953+rijal404@users.noreply.github.com>
2023-03-06 17:49:40 +0700

    Fix Orientation State

--- app/src/main/java/com/rijaldev/mygithub/presentation/detail/follow/FollowFragment.kt
+++ app/src/main/java/com/rijaldev/mygithub/presentation/detail/follow/FollowFragment.kt
@@ -1,11 +1,11 @@
 package com.rijaldev.mygithub.presentation.detail.follow
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.view.isVisible
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
